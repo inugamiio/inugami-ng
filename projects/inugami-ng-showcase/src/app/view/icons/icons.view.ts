5e985248e66113603b776f762a0410a4ba6f8bb3
Patrick Guillerm <pguillerm@smartwavesa.com>
2026-01-17 09:58:00 +0100

    #8 FEAT : create inu-code

--- projects/inugami-ng-showcase/src/app/view/icons/icons.view.ts
+++ projects/inugami-ng-showcase/src/app/view/icons/icons.view.ts
@@ -1,11 +1,15 @@
 import {Component, OnInit, signal, WritableSignal} from '@angular/core';
 import { InugamiIconsUtils, InuIcon} from 'inugami-icons';
-
+import {InuCode} from 'inugami-ng/components/inu-code';
+import {InuCite} from 'inugami-ng/components/inu-cite';
 @Component({
   templateUrl: './icons.view.html',
   styleUrls: ['./icons.view.scss'],
   imports: [
-    InuIcon
+    InuIcon,
+    InuCode,
+    InuCite,
+    InuCite
   ]
 })
 export class IconsView implements OnInit {
