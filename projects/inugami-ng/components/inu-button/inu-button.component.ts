fc6110b0a7690d656a4a936ce7655a69dc8af9ed
Patrick Guillerm <pguillerm@smartwavesa.com>
2026-03-15 14:19:57 +0100

    [#12] FEAT : create inu-input-text

--- projects/inugami-ng/components/inu-button/inu-button.component.ts
+++ projects/inugami-ng/components/inu-button/inu-button.component.ts
@@ -1,5 +1,6 @@
-import {Component, computed, input} from '@angular/core';
+import {AfterViewInit, Component, computed, effect, input, signal} from '@angular/core';
 import {InuIcon} from 'inugami-icons';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'inu-button',
@@ -8,17 +9,24 @@ import {InuIcon} from 'inugami-icons';
   templateUrl: './inu-button.component.html',
   styleUrl: './inu-button.component.scss',
 })
-export class InuButton {
+export class InuButton implements AfterViewInit{
 
   //==================================================================================================================
   // ATTRIBUTES
   //==================================================================================================================
   label = input<string | undefined | null>('');
   icon = input<string | null>(null);
+  iconSize = input<number>(1.2);
   type = input<string>('');
   link = input<boolean>(false);
   disabled = input<boolean>(false);
+  processing = input<boolean>(false);
+  processingIcon = input<string | null>(null);
 
+  _processIcon = computed<string|null>(()=>{
+    const result = this.processingIcon();
+    return result?result: this.icon();
+  } )
   _styleClass = computed<string>(() => {
     return [
       'inu-button',
@@ -27,5 +35,25 @@ export class InuButton {
       this.disabled()? 'disabled' : ''
     ].join(' ');
   })
+
+  iconStyleClass = signal<string>('inu-button-icon');
+
+  constructor() {
+    effect(() => {
+     this.iconStyleClass.set(this.computeIconStyleClass());
+    });
+  }
+  ngAfterViewInit(): void {
+    this.iconStyleClass.set(this.computeIconStyleClass());
+  }
+
+  computeIconStyleClass(){
+    return [
+      'inu-button-icon',
+      this.processing() ? 'rotate' : ''
+    ].join(' ');
+  }
+
+
 
 }
