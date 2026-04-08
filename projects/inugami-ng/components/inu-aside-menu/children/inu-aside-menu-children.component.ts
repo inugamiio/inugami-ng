ca9ce9bf249f9ea9311c2d9dfc64c1c344aa6cfe
Patrick Guillerm <pguillerm@smartwavesa.com>
2026-04-08 21:06:37 +0200

    [#97] FEAT : add router link fragment

--- projects/inugami-ng/components/inu-aside-menu/children/inu-aside-menu-children.component.ts
+++ projects/inugami-ng/components/inu-aside-menu/children/inu-aside-menu-children.component.ts
@@ -1,8 +1,9 @@
 import {Component, computed, inject, input} from '@angular/core';
 import {InuIcon} from 'inugami-icons';
-import {InuSiteLinkChildren} from 'inugami-ng/models'
-import {ActivatedRoute, RouterLink, RouterLinkActive} from '@angular/router'
+import {InuSiteLink, InuSiteLinkChildren} from 'inugami-ng/models'
+import {ActivatedRoute, NavigationEnd, Router, RouterLink, RouterLinkActive} from '@angular/router'
 import {toSignal} from '@angular/core/rxjs-interop'
+import {filter, map} from 'rxjs'
 
 @Component({
              selector   : 'inu-aside-menu-children',
@@ -15,9 +16,31 @@ export class InuAsideMenuChildren {
   //==================================================================================================================
   // ATTRIBUTES
   //==================================================================================================================
-  activeFragment = toSignal(inject(ActivatedRoute).fragment);
+  private activatedRoute    = inject(ActivatedRoute);
+  private router = inject(Router);
+  activeFragment    = toSignal(this.activatedRoute.fragment);
+  currentUrlString = toSignal(
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(() => this.router.url.split('?')[0])
+    ),
+    { initialValue: this.router.url.split('?')[0] }
+  );
   children          = input<InuSiteLinkChildren[]>([]);
   level             = input<number>(0);
   currentStyleClass = computed(() => ['inu-aside-menu-children', `level level-${this.level()}`]
     .join(' '));
+
+
+  showChildren(childLink: InuSiteLink): boolean {
+    console.log('showChildren',childLink)
+    if (!childLink.children || childLink.children.length == 0) {
+      return false;
+    }
+    if (childLink.forceShowChildren != undefined && childLink.forceShowChildren) {
+      return true;
+    }
+    const url = this.currentUrlString();
+    return url === childLink.path;
+  }
 }
