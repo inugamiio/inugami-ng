211eccd1732ee6d047e5c9ca31e9f50cdca168c1
Patrick Guillerm <pguillerm@smartwavesa.com>
2026-05-14 14:20:04 +0200

    [#108] FEAT : create inu-label component

--- projects/inugami-ng-showcase/src/mock/label.handler.ts
+++ projects/inugami-ng-showcase/src/mock/label.handler.ts
@@ -1,12 +1,16 @@
 import {http, HttpResponse} from 'msw'
+const baseHref = document.getElementsByTagName('base')[0]?.getAttribute('href') || '/';
 
 export const labelhandlers = [
 
   //====================================================================================================================
   // [GET] /api/label
   //====================================================================================================================
-  http.get('/api/label', async ({request}) => {
-    const response = await fetch('data/mock/label/api_label.get.json');
+  http.get('*/api/label', async ({request}) => {
+    const response = await fetch(`${baseHref}data/mock/label/api_label.get.json`.replace(/\/+/g, '/'));
+    if (!response.ok) {
+      return new HttpResponse('Mock file not found', { status: 404 });
+    }
     const data     = await response.json();
     return HttpResponse.json(data);
   }),
