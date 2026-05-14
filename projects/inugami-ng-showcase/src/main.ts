415a051ebee20a7c3d9eb10b79ce2064387afcbd
Patrick Guillerm <pguillerm@smartwavesa.com>
2026-05-14 14:05:27 +0200

    [#108] FEAT : create inu-label component

--- projects/inugami-ng-showcase/src/main.ts
+++ projects/inugami-ng-showcase/src/main.ts
@@ -1,7 +1,7 @@
 import {bootstrapApplication} from '@angular/platform-browser';
 import {appConfig} from './app/app.config';
 import {App} from './app/app';
-const baseHref = document.getElementsByTagName('base')[0]?.href || '/';
+const baseHref = document.getElementsByTagName('base')[0]?.getAttribute('href') || '/';
 
 async function prepareApp() {
   const { worker } = await import('./mock/browser');
