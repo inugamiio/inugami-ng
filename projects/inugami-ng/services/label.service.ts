8a24686f5b79cd3822aa4232ea13b72a91c84a0c
Patrick Guillerm <pguillerm@smartwavesa.com>
2026-05-27 07:29:04 +0200

    [#109] FEAT : fix services

--- projects/inugami-ng/services/label.service.ts
+++ projects/inugami-ng/services/label.service.ts
@@ -12,6 +12,7 @@ export function provideInuLabelService(implementation: Type<IInuLabelService> = 
 }
 
 const TTL: number = 60000 * 60;
+const LABELS : Record<string, Record<string, InuLabelAPI>> = {};
 
 @Injectable({providedIn: 'root'})
 export class InuLabelService implements IInuLabelService {
@@ -23,7 +24,7 @@ export class InuLabelService implements IInuLabelService {
   httpClient    = inject(HttpClient);
   cacheServices = inject(InuCacheServices);
 
-  labels             = signal<Record<string, Record<string, InuLabelAPI>>>({});
+
   supportedLanguages = signal<string[]>([]);
   url                = signal<string>('api/label');
   language           = signal<string>('EN');
@@ -40,7 +41,7 @@ export class InuLabelService implements IInuLabelService {
     const data: InuLabelResponse | undefined | null = this.cacheServices.getTTL(`InuLabelService_${this.url()}`);
     if (data) {
       this.init(data);
-      return of({});
+      return of(data);
     } else {
       return this.httpClient.get<InuLabelResponse>(this.url())
         .pipe(tap(res => {
@@ -57,7 +58,7 @@ export class InuLabelService implements IInuLabelService {
       let values = labels[lang];
       if (!values) {
         values       = {};
-        labels[lang] = values;
+        LABELS[lang] = values;
       }
       for (let item of res[lang]) {
         values[item.key] = item;
@@ -65,9 +66,7 @@ export class InuLabelService implements IInuLabelService {
 
 
     });
-
-    this.labels.set(labels);
-    this.supportedLanguages.set(Object.keys(res));
+    this.supportedLanguages.set(Object.keys(LABELS));
   }
 
 
@@ -81,12 +80,11 @@ export class InuLabelService implements IInuLabelService {
     this.url.set(apiUrl);
   }
   findLabel(key: string | undefined): InuLabelAPI | undefined {
-    const currentLabels = this.labels();
     const currentLang   = this.lang();
 
-    let labelsForLang = currentLabels[currentLang];
+    let labelsForLang = LABELS[currentLang];
     if (!labelsForLang && this.supportedLanguages().length > 0) {
-      labelsForLang = currentLabels[this.supportedLanguages()[0]];
+      labelsForLang = LABELS[this.supportedLanguages()[0]];
     }
 
     if (!key || !labelsForLang) return undefined;
