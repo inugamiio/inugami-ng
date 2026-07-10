7549f49524d19e19e5947b6d46509c1a5c9bbf2c
Patrick Guillerm <pguillerm@smartwavesa.com>
2026-07-10 17:08:05 +0200

    [#20] FEAT :  create toggle component

---
+++ projects/inugami-ng-showcase/src/app/view/forms/inu-toggle/inu-toggel.view.ts
@@ -0,0 +1,83 @@
+import {Component, signal} from '@angular/core';
+import {InuPanelTab, InuPanelTabs} from 'inugami-ng/components/inu-panel-tabs';
+import {
+  InuTableFlex,
+  InuTableFlexCell,
+  InuTableFlexHeader,
+  InuTableFlexRow
+} from 'inugami-ng/components/inu-table-flex';
+import {takeUntilDestroyed, toObservable} from '@angular/core/rxjs-interop';
+import {debounceTime, distinctUntilChanged} from 'rxjs';
+import {form, FormField, required,} from '@angular/forms/signals';
+import {InuCode} from 'inugami-ng/components/inu-code';
+import {InuToggle} from 'inugami-ng/components/inu-toggle';
+
+
+
+interface MyFormModel {
+  sms: boolean;
+  email: boolean;
+  mail: boolean;
+  inApp: boolean;
+}
+
+@Component({
+             templateUrl: './inu-toggle.view.html',
+             styleUrls  : ['./inu-toggle.view.scss'],
+             imports: [
+               InuTableFlex,
+               InuTableFlexHeader,
+               InuTableFlexRow,
+               InuTableFlexCell,
+               InuPanelTabs,
+               InuPanelTab,
+               InuCode,
+               InuToggle,
+               FormField
+             ]
+           })
+export class InuToggleView {
+
+  data      = signal<string>('');
+  formModel = signal<MyFormModel>({
+                                    sms: false,
+                                    email: true,
+                                    inApp: false,
+                                    mail: false
+                                  });
+
+  myForm = form(this.formModel, (path) => {
+    required(path.sms);
+  });
+
+
+  genericT              = signal<string>('<T>');
+  genericTList          = signal<string>('<T>[]');
+  genericSearchProvider = signal<string>('SearchProvider<InuSelectListSearchRequest, T>');
+
+
+  //==================================================================================================================
+  // INIT
+  //==================================================================================================================
+  constructor() {
+    toObservable(this.formModel)
+      .pipe(
+        debounceTime(200),
+        distinctUntilChanged(),
+        takeUntilDestroyed()
+      )
+      .subscribe(value => {
+        this.onValueChanged(value)
+      });
+  }
+
+
+  //==================================================================================================================
+  // EVENTS
+  //==================================================================================================================
+  private onValueChanged(value: MyFormModel) {
+    this.data.set(JSON.stringify(value, null, 4));
+  }
+
+
+}
---
+++ projects/inugami-ng/components/inu-toggle/public-api.ts
@@ -0,0 +1,2 @@
+export * from './inu-toggle.component';
+
